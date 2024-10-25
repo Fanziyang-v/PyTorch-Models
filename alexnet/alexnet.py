6bf3b6ea57850caf9d27c68a996d2f734d2602d4
ziyangfan <1229417754@qq.com>
2024-10-25 12:25:59 +0800

    Set inplace parameter value of ReLU activation to True.

--- alexnet/alexnet.py
+++ alexnet/alexnet.py
@@ -18,17 +18,17 @@ class AlexNet(nn.Module):
         super(AlexNet, self).__init__()
         self.model = nn.Sequential(
             nn.Conv2d(num_channels, 96, kernel_size=11, stride=4, padding=2),  # 55x55
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),  # 27x27
             nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),  # 27x27
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),  # 13x13
             nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),  # 13x13
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),  # 13x13
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),  # 13x13
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),  # 6x6
             nn.Flatten(),
             nn.Linear(256 * 6 * 6, 4096),
