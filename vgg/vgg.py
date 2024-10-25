ddd0e568c5ad93fb990244332a66929c88940e4f
ziyangfan <1229417754@qq.com>
2024-10-25 12:14:26 +0800

    add batchnorm option in VGG Net.

--- vgg/vgg.py
+++ vgg/vgg.py
@@ -4,14 +4,20 @@ from vgg_block import VGGBlock
 
 
 class VGG(nn.Module):
-    def __init__(self, num_channels: int, num_classes: int, layers: list[int]) -> None:
+    def __init__(
+        self,
+        num_channels: int,
+        num_classes: int,
+        layers: list[int],
+        use_batchnorm: bool = False,
+    ) -> None:
         super(VGG, self).__init__()
         self.model = nn.Sequential(
-            VGGBlock(num_channels, 64, layers[0]),
-            VGGBlock(64, 128, layers[1]),
-            VGGBlock(128, 256, layers[2]),
-            VGGBlock(256, 512, layers[3]),
-            VGGBlock(512, 512, layers[4]),
+            VGGBlock(num_channels, 64, layers[0], use_batchnorm),
+            VGGBlock(64, 128, layers[1], use_batchnorm),
+            VGGBlock(128, 256, layers[2], use_batchnorm),
+            VGGBlock(256, 512, layers[3], use_batchnorm),
+            VGGBlock(512, 512, layers[4], use_batchnorm),
             nn.Flatten(),
             nn.Linear(512 * 7 * 7, 4096),
             nn.ReLU(inplace=True),
@@ -21,6 +27,6 @@ class VGG(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, num_classes),
         )
-    
+
     def forward(self, images: Tensor):
         return self.model(images)
--- vgg/vgg_block.py
+++ vgg/vgg_block.py
@@ -4,7 +4,14 @@ from torch import Tensor
 
 class VGGBlock(nn.Module):
     """VGG Block."""
-    def __init__(self, in_channels: int, out_channels: int, num_convs: int) -> None:
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        num_convs: int,
+        use_batchnorm: bool = False,
+    ) -> None:
         """Initialize a VGG Block.
 
         Args:
@@ -13,9 +20,19 @@ class VGGBlock(nn.Module):
             num_convs (int): number of convolution layers.
         """
         super(VGGBlock, self).__init__()
-        layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)]
+        layers = [
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        ]
+        if use_batchnorm:
+            layers.append(nn.BatchNorm2d(out_channels))
         for _ in range(num_convs - 1):
-            layers.append(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1))
+            layers.append(
+                nn.Conv2d(
+                    out_channels, out_channels, kernel_size=3, stride=1, padding=1
+                )
+            )
+            if use_batchnorm:
+                layers.append(nn.BatchNorm2d(out_channels))
         layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
         self.block = nn.Sequential(*layers)
 
